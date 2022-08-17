82000a51041897bc02c2dfe4eba713e3ac4d2e90
Navid TehraniFar <navid@dapperlabs.com>
2022-08-17 15:19:14 -0700

    500 status for errors

--- internal/api/process.go
+++ internal/api/process.go
@@ -25,6 +25,7 @@ func (api *Api) ProcessRepoHandler(w http.ResponseWriter, r *http.Request) {
 	result, err := api.kit.Process(params.Owner, params.Repo, params.Network)
 	if err != nil {
 		fmt.Printf("Error in ProcessRepo(%s, %s, %s) -> %s\n", params.Owner, params.Repo, params.Network, err)
+		w.WriteHeader(500)
 		json.NewEncoder(w).Encode(&JSONError{Error: err.Error()})
 		return
 	}
